b2db96052780c67820930c50e21f44ede97b228e
PyYoshi <myoshi321go@gmail.com>
2017-01-13 11:30:23 +0900

    add ETLFormat

--- formats/common.go
+++ formats/common.go
@@ -1,5 +1,20 @@
 package formats
 
-// type Format interface {
-// 	ReadFile() error
-// }
+type ETLFormat string
+
+const (
+	ETLFormat1  ETLFormat = "1"
+	ETLFormat2  ETLFormat = "2"
+	ETLFormat3  ETLFormat = "3"
+	ETLFormat4  ETLFormat = "4"
+	ETLFormat5  ETLFormat = "5"
+	ETLFormat6  ETLFormat = "6"
+	ETLFormat7  ETLFormat = "7"
+	ETLFormat8b ETLFormat = "8b"
+	ETLFormat8g ETLFormat = "8g"
+	ETLFormat9b ETLFormat = "9b"
+	ETLFormat9g ETLFormat = "9g"
+)
+
+type Record interface {
+}
